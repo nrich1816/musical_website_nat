51d0f40e25c8ca40d6a2cb6e54d5e60208220186
aaron ford <aaronswford@hotmail.co.uk>
2025-07-05 23:36:56 +0100

    messed up files

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Website loaded!");
+
+function toggleNavbar() {
+    const navbar = document.getElementById("navbar");
+    const burger = document.querySelector(".navbar-burger");
+    navbar.classList.contains("is-active") ?
+        navbar.classList.remove("is-active") :
+        navbar.classList.add("is-active");
+    burger.classList.contains("is-active") ?
+        burger.classList.remove("is-active") :
+        burger.classList.add("is-active");
+}
